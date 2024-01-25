fd8156dde548c53537c1dafc2caabb94de93de5e
Kevin <18002263+Darth-Revan@users.noreply.github.com>
2024-01-25 10:58:12 +0100

    Remote Chek AzStorAcMinTLS12, as it is a duplicate of AzStorAcMinTLSVersion

--- AzRanger/Checks/Rules/AzStorAcMinTLSVersion.cs
+++ AzRanger/Checks/Rules/AzStorAcMinTLSVersion.cs
@@ -1,15 +1,12 @@
 ﻿using AzRanger.Models;
 using AzRanger.Models.AzMgmt;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AzRanger.Checks.Rules
 {
     internal class AzStorAcMinTLSVersion : BaseCheck
     {
+        static readonly string MIN_TLS_VERSION = "TLS1_2";
+
         public override CheckResult Audit(Tenant tenant)
         {
             bool passed = true;
@@ -18,7 +15,7 @@ namespace AzRanger.Checks.Rules
             {
                 foreach(StorageAccount account in sub.Resources.StorageAccounts)
                 {
-                    if(account.properties.minimumTlsVersion != "TLS1_2")
+                    if(account.properties.minimumTlsVersion != MIN_TLS_VERSION)
                     {
                         passed = false;
                         this.AddAffectedEntity(account);
